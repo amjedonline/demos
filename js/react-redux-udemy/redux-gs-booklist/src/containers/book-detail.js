8c8688dd578e1fe065e67fd0032951474d74aef3
amjedonline <amjedonline@gmail.com>
2016-04-27 18:31:10 +0200

    Connecting the BookDetail component to receive states, and set the properties.

--- js/react-redux-udemy/redux-gs-booklist/src/containers/book-detail.js
+++ js/react-redux-udemy/redux-gs-booklist/src/containers/book-detail.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
   render() {
     return (
       <div>Book Detail!</div>
     );
   }
 }
+
+// What data this container ll have ?
+function mapStateToProps(state){
+  return {
+    book: state.activeBook
+  }
+}
+
+export default connect(mapStateToProps)(BookDetail)
