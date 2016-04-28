f58004465aa129632d6fc5dff00c35d9da5eea30
amjedonline <amjedonline@gmail.com>
2016-04-28 18:14:13 +0200

    Adding ActionCreator, bindingActionCreators to container properties.

--- js/react-redux-udemy/weather-forecast-ajax/src/actions/index.js
+++ js/react-redux-udemy/weather-forecast-ajax/src/actions/index.js
@@ -1,1 +1,20 @@
-const API_KEY='145af9d173b0c6c194cceb2c609f5708';
+import axios from 'axios';
+
+const API_KEY = '145af9d173b0c6c194cceb2c609f5708';
+// ES6 template strings
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+ export function fetchWeather(city) {
+   const url = `${ROOT_URL}&q=${city},us`
+
+   // returns a promise
+   const request = axios.get(url);
+
+   return {
+     type: FETCH_WEATHER,
+     payload: request
+   }
+ }
--- js/react-redux-udemy/weather-forecast-ajax/src/containers/search_bar.js
+++ js/react-redux-udemy/weather-forecast-ajax/src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props);
 
@@ -8,6 +11,7 @@ export default class SearchBar extends Component {
 
     // bind the function to searchBar.
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -18,6 +22,9 @@ export default class SearchBar extends Component {
   onFormSubmit(event) {
     // stop the form submission
     event.preventDefault();
+    // get fetch the weather
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
   }
 
   render() {
@@ -37,3 +44,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
