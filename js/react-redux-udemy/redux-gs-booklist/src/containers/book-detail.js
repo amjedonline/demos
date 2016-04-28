f7d876be445d6bad4ea5419c204594b698dceecb
amjedonline <amjedonline@gmail.com>
2016-04-28 16:15:15 +0200

    Adding more properties to books.

--- js/react-redux-udemy/redux-gs-booklist/src/containers/book-detail.js
+++ js/react-redux-udemy/redux-gs-booklist/src/containers/book-detail.js
@@ -3,13 +3,22 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if(!this.props.book) {
+      return <div>Select a book to get started.</div>
+    }
+
     return (
-      <div>Book Detail!</div>
+      <div>
+        <h3>Details for:</h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
+      </div>
     );
   }
 }
 
 // What data this container ll have ?
+// Is this to receive state (changed eventually) and map to the props to be used in the UI/Component.
 function mapStateToProps(state){
   return {
     book: state.activeBook
