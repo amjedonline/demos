3a2f78f4c0e35f70f7826714319eb19d533a6158
amjedonline <amjedonline@gmail.com>
2016-05-26 20:30:44 +0200

    Adding router.

--- js/react-redux-udemy/client-side-auth/src/index.js
+++ js/react-redux-udemy/client-side-auth/src/index.js
@@ -6,10 +6,15 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
