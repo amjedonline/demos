e69b32bae937af88f2961886b4664ac471162b9b
amjedonline <amjedonline@gmail.com>
2016-05-30 11:40:44 +0200

    Adding the auto login feature.

--- js/react-redux-udemy/client-side-auth/src/index.js
+++ js/react-redux-udemy/client-side-auth/src/index.js
@@ -11,11 +11,22 @@ import Signout from './components/auth/signout';
 import Feature from './components/feature';
 import RequireAuth from './components/auth/require_auth';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {AUTH_USER} from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const token = localStorage.getItem('token');
+
+const store = createStoreWithMiddleware(reducers);
+
+//if we have a token, consider the user to be signed in
+if(token){
+  // we need to update the application state
+  // dispatch method is the property of the store
+  store.dispatch({type: AUTH_USER});
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <Route path="signin" component={Signin}/>
